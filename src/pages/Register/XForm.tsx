86e2ac6b6c55a06c4127af82814d1d8554d5fae2
hn1987 <hn1987@126.com>
2019-07-06 16:08:32 +0800

    贷款金额

--- src/pages/Register/XForm.tsx
+++ src/pages/Register/XForm.tsx
@@ -151,13 +151,12 @@ let _XForm = (props: XFormProps) => {
                 rules: [{ required: true, message: '请选择金额' }]
               })(
                 <Select placeholder="金额" suffixIcon={<Icon type="caret-down" />}>
-                  <Option value="5">5万</Option>
-                  <Option value="10">10万</Option>
-                  <Option value="15">15万</Option>
-                  <Option value="20">20万</Option>
-                  <Option value="30">30万</Option>
-                  <Option value="50">50万</Option>
-                  <Option value="100">100万</Option>
+                  <Option value="1">5万以下</Option>
+                  <Option value="2">5-10万</Option>
+                  <Option value="3">10-20万</Option>
+                  <Option value="4">20-50万</Option>
+                  <Option value="5">50-100万</Option>
+                  <Option value="6">100万以上</Option>
                 </Select>
               )}
             </Form.Item>

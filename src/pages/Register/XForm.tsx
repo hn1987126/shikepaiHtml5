ea96cff1b34ef34e14dffb2a6f8d4ed1a309ecff
chenyi363 <chenyi9@jd.com>
2019-07-17 18:04:28 +0800

    money

--- src/pages/Register/XForm.tsx
+++ src/pages/Register/XForm.tsx
@@ -147,7 +147,7 @@ let _XForm = (props: XFormProps) => {
           <div className="flexWrap">
             <Form.Item label="借多少:" hasFeedback {...formTopItemLayout}>
               {getFieldDecorator('loan', {
-                initialValue: '1',
+                initialValue: '2',
                 rules: [{ required: true, message: '请选择金额' }]
               })(
                 <Select placeholder="金额" suffixIcon={<Icon type="caret-down" />}>

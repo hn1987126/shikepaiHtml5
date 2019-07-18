4f2f960419030234d6f5c842c3b69bb1e9959fe2
chenyi363 <chenyi9@jd.com>
2019-07-18 16:13:48 +0800

    share

--- src/pages/InvitationDetail/index.tsx
+++ src/pages/InvitationDetail/index.tsx
@@ -95,7 +95,7 @@ let InvitationDetail = () => {
       let timer = setTimeout(() => {
         if (isAndroid()) {
           setShowDownloadLoading(false);
-          window.location.href = 'http://www.shikepai.com/upload/brokerApp/skp_V3.1.0.apk';
+          window.location.href = 'http://www.shikepai.com/upload/brokerApp/skp_V3.1.1.apk';
         } else {
           setShowDownloadLoading(false);
           window.location.href =
